2a9d1d9d8fb53ab38a6db87cca1d152a726de30a
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-07-02 15:29:27 -0500

    refactor: Chage code and animations of navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useEffect, useRef, useState } from 'react';
-import { usePathname } from 'next/navigation';
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const OPTIONS = [
 	{
@@ -32,102 +32,93 @@ const OPTIONS = [
 	},
 	{
 		text: 'Contact',
-		icon: '/icons/Phone.svg',
+		icon: '/icons/Contact.svg',
 		alt: 'Contact Icon',
 		href: '',
 	},
 ];
 
 function Navbar() {
-	const path = usePathname();
 	const [expand, setExpand] = useState(false);
-	const [naSizes, setNaSizes] = useState<{
-		noexpanded: number;
-		expanded: number;
-	}>();
-	const [init, setInit] = useState(true);
-
-	const iconRef = useRef<HTMLImageElement>(null);
-	const optRef = useRef<HTMLDivElement>(null);
-
-	useEffect(() => {
-		if (!(iconRef.current && optRef.current)) return;
-
-		setNaSizes({
-			noexpanded: iconRef.current.clientWidth,
-			expanded: optRef.current.clientWidth,
-		});
-		setInit(false);
-	}, []);
+	const [selected, setSelected] = useState(0);
 
 	return (
-		<div
-			className="absolute grid grid-cols-2 bg-white rounded-3xl top-1/2 -translate-x-1/2 -translate-y-1/2"
-			style={{
-				visibility: init ? 'hidden' : 'visible',
-			}}
-		>
-			<nav
-				className="col-start-2 py-6 px-2 flex flex-col"
+		<div className="fixed h-full w-auto flex items-center z-50">
+			<motion.div
+				className="grid grid-cols-2 bg-white rounded-3xl z-50"
 				onMouseEnter={() => setExpand(true)}
 				onMouseLeave={() => setExpand(false)}
+				style={{
+					x: '-50%',
+				}}
+				animate={
+					!expand
+						? {
+								width: '8rem',
+						  }
+						: {
+								width: '18rem',
+						  }
+				}
+				transition={{
+					duration: 0.3,
+				}}
 			>
-				{OPTIONS.map((opt, index) => {
-					return (
-						<Link
-							key={index}
-							href={opt.href}
-							className="rounded-xl focus:outline-none my-2 px-2 py-1 transition-all duration-300 hover:bg-[rgba(205,205,205,0.34)]"
-							style={
-								path === opt.href
-									? {
-											backgroundColor: 'black',
-									  }
-									: {}
-							}
-						>
-							<div
-								className="inline-flex items-center overflow-hidden transition-all"
-								ref={optRef || null}
-								style={{
-									width:
-										!init && naSizes
-											? expand
-												? naSizes.expanded
-												: naSizes.noexpanded
-											: '',
-								}}
+				<nav className="col-start-2 py-6 px-2 flex flex-col gap-3">
+					{OPTIONS.map((opt, index) => {
+						return (
+							<Link
+								key={index}
+								href={opt.href}
+								className={
+									(index != selected
+										? 'transition-all duration-300 hover:bg-[rgba(205,205,205,0.34)]'
+										: '') +
+									' relative rounded-xl focus:outline-none px-2 py-1'
+								}
+								onClick={() => setSelected(index)}
 							>
-								<Image
-									src={opt.icon}
-									alt={opt.alt}
-									className="h-8 w-8"
-									width={0}
-									height={0}
-									ref={iconRef}
-									style={{
-										filter:
-											path === opt.href
-												? 'invert(1)'
-												: '',
-									}}
-								/>
-								<span
-									className="ml-2"
-									style={{
-										filter:
-											path === opt.href
-												? 'invert(1)'
-												: '',
-									}}
-								>
-									{opt.text}
-								</span>
-							</div>
-						</Link>
-					);
-				})}
-			</nav>
+								{index == selected && (
+									<motion.span
+										layout
+										layoutId="hover"
+										className="absolute bg-white w-full h-full left-0 top-0 rounded-xl mix-blend-difference z-10"
+									/>
+								)}
+								<div className="relative text-black inline-flex items-center gap-2">
+									<Image
+										src={opt.icon}
+										alt={opt.alt}
+										className="h-8 w-8"
+										width={0}
+										height={0}
+									/>
+									<AnimatePresence>
+										{expand && (
+											<motion.span
+												initial={{
+													opacity: 0,
+												}}
+												animate={{
+													opacity: 1,
+												}}
+												exit={{
+													opacity: 0,
+												}}
+												transition={{
+													duration:0.1
+												}}
+											>
+												{opt.text}
+											</motion.span>
+										)}
+									</AnimatePresence>
+								</div>
+							</Link>
+						);
+					})}
+				</nav>
+			</motion.div>
 		</div>
 	);
 }
