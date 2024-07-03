0eada38ee3e6b0af63f5757ba35362c71bb5263f
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-07-02 23:56:45 -0500

    refactor: update logo animation with framer motion

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,86 @@
+'use client';
+
 import React from 'react';
-import style from '@/styles/Header.module.css';
 import Image from 'next/image';
+import { Variants, motion } from 'framer-motion';
+
+const parentVariants: Variants = {
+	visible: {
+		border: '1.5px rgb(255,255,255,0) solid',
+		transition: {
+			duration: 1,
+		},
+	},
+	hidden: {
+		borderColor: 'rgb(255,255,255,1)',
+		transition: {
+			duration: 1.8,
+			when: 'afterChildren',
+			delayChildren: 2,
+		},
+	},
+};
+
+const textVariants: Variants = {
+	visible: {
+		width: 'auto',
+		opacity: 1
+	},
+	hidden: {
+		width: '0',
+		opacity: 0,
+		marginRight: 0,
+		transition: {
+			duration: 0.4,
+			ease: 'easeOut'
+		}
+	},
+};
 
 function Header() {
 	return (
 		<header className="fixed flex items-center w-full h-14 px-4">
-			<div
-				className={`${style.reducer} inline-block mr-auto border-[2px] border-transparent text-white rounded-full p-1`}
+			<motion.div
+				className={`inline-block mr-auto text-white rounded-full p-1`}
+				variants={parentVariants}
+				initial="visible"
+				animate="hidden"
 			>
-				E<span className="inline-block w-16">dinson</span>N
-				<span className="inline-block">oriega</span>
-			</div>
+				<h1 className='flex justify-center items-center'>
+					E
+					<motion.span
+						className="inline-block mr-1 overflow-hidden"
+						variants={textVariants}
+					>
+						dinson
+					</motion.span>
+					N
+					<motion.span
+						className="inline-block overflow-hidden"
+						variants={textVariants}
+					>
+						oriega
+					</motion.span>
+				</h1>
+			</motion.div>
 			<div className="inline-flex ml-auto gap-2">
 				<Image
 					src={'/icons/Phone.svg'}
-					className='h-11 w-11'
+					className="h-11 w-11"
 					alt="Linkedin Icon"
 					width={0}
 					height={0}
 				/>
 				<Image
 					src={'/icons/Mail.svg'}
-					className='h-11 w-11'
+					className="h-11 w-11"
 					alt="Linkedin Icon"
 					width={0}
 					height={0}
 				/>
 				<Image
 					src={'/icons/Linkedin.svg'}
-					className='h-11 w-11'
+					className="h-11 w-11"
 					alt="Linkedin Icon"
 					width={0}
 					height={0}
