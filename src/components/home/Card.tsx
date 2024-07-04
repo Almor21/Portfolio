9b33fb3910f2f31874038b9f961c215170963524
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-07-04 14:08:32 -0500

    style: change border profile animation

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -3,7 +3,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import TypeWriter from './TypeWriter';
-import { motion } from 'framer-motion';
 import { useStoreBackground } from '@/stores/useStore';
 
 function Card() {
@@ -37,28 +36,19 @@ function Card() {
 
 	return (
 		<div className="absolute grid grid-cols-[repeat(2,auto)] items-center gap-4 top-[50vh] left-[50vw] -translate-x-1/2 -translate-y-1/2">
-			<motion.div
-				className="relative h-[40vh] w-[40vh] box-border rounded-full overflow-hidden rotate-45 shadow-[2px_2px_8px_2px_rgb(0,0,0,0.5)]"
-				style={{
-					borderColor: 'black black white white',
-				}}
-				animate={{
-					borderWidth: 3,
-				}}
-				transition={{
-					duration: 0.3,
-					delay: 1,
-					ease: 'easeOut',
-				}}
+			<div
+				className="relative h-[40vh] w-[40vh] box-border rounded-full overflow-hidden rotate-45 p-1 shadow-[2px_2px_8px_2px_rgb(0,0,0,0.5)] backdrop-invert"
 			>
-				<Image
-					src="/VoidPhoto.jpeg"
-					alt="Profile image"
-					fill={true}
-					className="-rotate-45"
-					ref={imageRef || null}
-				/>
-			</motion.div>
+				<div className='relative h-full w-full'>
+					<Image
+						src="/VoidPhoto.jpeg"
+						alt="Profile image"
+						fill={true}
+						className="-rotate-45 rounded-full"
+						ref={imageRef || null}
+					/>
+				</div>
+			</div>
 			<div className="relative grid grid-rows-[repeat(2,1fr)]">
 				<Image
 					src={'/Presentation.svg'}
