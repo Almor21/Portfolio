32b11c3a93451c1c858bf150e3e9721c0065395f
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-07-04 21:44:30 -0500

    refactor: change global styles

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,22 +2,19 @@ import React from 'react';
 import Image from 'next/image';
 import Title from '@/components/about/Title';
 import Separator from '@/components/about/Separator';
-import style from '@/styles/about.module.css';
 
 function AboutPage() {
 	return (
 		<div className="grid grid-cols-[repeat(2,1fr)] gap-16">
 			<div className="justify-self-end w-1/2 flex flex-col items-center text-white">
-				<div
-					className={`${style.rotate} p-1 border-2 border-[white_transparent_white_white] rounded-full transition-all duration-500`}
-				>
-					<div className="p-1 border-2 border-[white_white_white_transparent] rounded-full overflow-hidden transition-all duration-500">
-						<div className='relative h-28 w-28'>
+				<div className="p-1 border-2 border-[white_transparent_white_white] rounded-full transition-all duration-500 group hover:-rotate-180">
+					<div className="p-1 border-2 border-[white_white_white_transparent] rounded-full overflow-hidden transition-all duration-500 group-hover:rotate-[360deg]">
+						<div className="relative h-28 w-28">
 							<Image
 								src={'/VoidPhoto.jpeg'}
 								alt="Profile image"
 								fill={true}
-								className="rounded-full filter grayscale transition-all duration-500"
+								className="rounded-full filter grayscale transition-all duration-500 group-hover:-rotate-180 group-hover:grayscale-0"
 							/>
 						</div>
 					</div>
