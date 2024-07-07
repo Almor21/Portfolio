60b7ed69eb7fb2965ee6cc8289d7490239b2a9f5
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-07-07 01:56:23 -0500

    refactor: delete route to get data with name

--- src/app/api/data/[name]/route.ts
+++
@@ -1,7 +0,0 @@
-import { readData } from '@/utils/dataHandler';
-
-export function GET({ params }: { params: { name: string } }) {
-	const { categories, data } = readData();
-
-	return Response.json(data);
-}
