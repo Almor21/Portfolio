ff954dfc24364bf2dcd15e519ac90ab183b6cc8a
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-07-14 02:11:05 -0500

    fix[background]: bug size for large screens

--- src/components/Backgroud.tsx
+++ src/components/Backgroud.tsx
@@ -1,26 +1,32 @@
 'use client';
 
 import { useStoreBackground } from '@/stores/useStore';
-import React from 'react';
+import React, { useRef } from 'react';
 import { motion } from 'framer-motion';
 
 function Backgroud() {
 	const XBackground = useStoreBackground((state) => state.XBackground);
+	const divRef = useRef<HTMLDivElement>(null);
 
 	return (
 		<motion.div
 			className="absolute bg-black h-full top-0 -z-50"
+			ref={divRef}
 			initial={{
 				width: 0,
 			}}
 			animate={{
-				width: XBackground.at(-1) === '%' ? XBackground : parseInt(XBackground),
+				width:
+					XBackground.at(-1) === '%'
+						? XBackground
+						: parseInt(XBackground) -
+						  (divRef.current?.getBoundingClientRect().left ?? 0),
 			}}
 			transition={{
 				type: 'spring',
 				duration: 0.8,
 			}}
-		></motion.div>
+		/>
 	);
 }
 
