9cb342d2d3390abed2749da8b0fc8d94e6cb1905
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-07-14 02:12:55 -0500

    feat[about]: add description, information and transitions

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,62 +1,109 @@
 import React from 'react';
 import Image from 'next/image';
-import Title from '@/components/about/Title';
-import Separator from '@/components/about/Separator';
+import Section from '@/components/about/Section';
 import MoveBackground from '@/components/MoveBackground';
+import DivTransition from '@/components/DivTransition';
 
 function AboutPage() {
 	return (
-		<MoveBackground createDiv width="50%" className="gap-16">
-			<div className="justify-self-end w-1/2 flex flex-col items-center text-white">
-				<div className="p-1 border-2 border-[white_transparent_white_white] rounded-full transition-all duration-500 group hover:-rotate-180">
-					<div className="p-1 border-2 border-[white_white_white_transparent] rounded-full overflow-hidden transition-all duration-500 group-hover:rotate-[360deg]">
-						<div className="relative h-28 w-28">
-							<Image
-								src={'/VoidPhoto.jpeg'}
-								alt="Profile image"
-								fill={true}
-								className="rounded-full filter grayscale transition-all duration-500 group-hover:-rotate-180 group-hover:grayscale-0"
-							/>
+		<DivTransition>
+			<MoveBackground createDiv width="50%">
+				<div className="justify-self-end flex flex-col items-center text-white">
+					<div className="p-1 border-2 border-[white_transparent_white_white] rounded-full transition-all duration-500 group hover:-rotate-180">
+						<div className="p-1 border-2 border-[white_white_white_transparent] rounded-full overflow-hidden transition-all duration-500 group-hover:rotate-[360deg]">
+							<div className="relative h-36 w-36">
+								<Image
+									src={'/VoidPhoto.jpeg'}
+									alt="Profile image"
+									fill={true}
+									className="rounded-full filter grayscale transition-all duration-500 group-hover:-rotate-180 group-hover:grayscale-0"
+								/>
+							</div>
 						</div>
 					</div>
+					<p className="my-8 px-20 text-justify">
+						Edinson Noriega, I am 20 years old and I am a{' '}
+						<span className="font-bold">self-taught</span>,
+						<span className="font-bold"> active</span> and{' '}
+						<span className="font-bold">creative</span> person. I
+						enjoy learning and exploring new skills constantly.
+						<br />
+						<br />
+						Currently, I am a{' '}
+						<span className="font-bold">
+							Systems Engineering
+						</span>{' '}
+						student and{' '}
+						<span className="font-bold">Frontend developer</span>,
+						in the process of becoming a Fullstack developer. I have
+						solid knowledge in both{' '}
+						<span className="font-bold">Frontend</span> and{' '}
+						<span className="font-bold">Backend</span>, although I&apos;m
+						more inclined towards frontend.
+					</p>
 				</div>
-				<p className="my-8">
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-					Eius repudiandae esse, maiores unde voluptas cum id et nihil
-					laudantium exercitationem aliquam ipsa facere quibusdam
-					corporis voluptatem labore accusamus qui quasi?
-				</p>
-			</div>
-			<div className="justify-self-start w-9/12">
-				<Title>Activities</Title>
-				<Separator size={1} />
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Illo, consectetur consequuntur praesentium nulla asperiores
-					nihil. Dolores quam quidem debitis aspernatur asperiores
-					facilis aliquam mollitia temporibus alias maxime, quae enim
-					nobis?
-				</p>
-				<Title>Education</Title>
-				<Separator size={2} />
-				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-					Totam, necessitatibus? Maiores repellat non deleniti eius
-					facere. Perspiciatis quibusdam eaque officia. Nisi soluta
-					cum expedita veniam perspiciatis numquam, dolores dolore
-					blanditiis!
-				</p>
-				<Title>Achievements</Title>
-				<Separator size={3} />
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-					Quis obcaecati expedita esse inventore sed magni natus,
-					earum dolores vitae provident repudiandae quod
-					exercitationem, repellendus minus neque velit quo, sunt
-					explicabo!
-				</p>
-			</div>
-		</MoveBackground>
+				<div className="justify-self-start px-12 text-xs">
+					<Section title="Activities" widthSeparator={1}>
+						<span className="font-bold text-sm">
+							Private lessons
+						</span>
+						<br />
+						<span className="font-medium text-sm">Freelance</span>
+						<br />
+						<ul>
+							<li>
+								Freelance tutor of engineering, mathematics, and
+								programming for university students.
+							</li>
+						</ul>
+						<span className="font-bold text-sm">Board Member</span>
+						<br />
+						<span className="font-medium text-sm">
+							ACM Uninorte
+						</span>
+						<br />
+						<ul>
+							<li>
+								Involved in the coordination and decision making
+								process for the development of events
+							</li>
+						</ul>
+					</Section>
+					<Section title="Education" widthSeparator={2}>
+						<span className="font-bold text-sm">
+							Systems and Computer Engineering
+						</span>
+						<br />
+						<span className="font-bold">
+							Universidad del Norte.{' '}
+						</span>{' '}
+						Barranquilla, Colombia | Jan 2021 - present (8th
+						semester).
+						<br />
+						<br />
+						<span className="font-bold text-sm">High School</span>
+						<br />
+						<span className="font-bold">
+							Institución Educativa San Marcos.{' '}
+						</span>{' '}
+						San Marcos | Jan 2015 - Nov 2020
+						<ul>
+							<li>Outstanding student with excellent grades.</li>
+							<li>
+								Winner of academic competitions within the
+								institution.
+							</li>
+						</ul>
+					</Section>
+					<Section title="Achievements" widthSeparator={3}>
+						<span className="font-bold text-sm">
+							Generation E Excellence Scholarship - ICFES 2020
+							Tests
+						</span>
+					</Section>
+				</div>
+			</MoveBackground>
+		</DivTransition>
 	);
 }
 
---
+++ src/components/about/Section.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Merriweather } from 'next/font/google';
+
+const merriweather = Merriweather({ weight: '400', subsets: ['latin'] });
+
+function Section({
+	children,
+	title,
+	widthSeparator,
+}: {
+	children: React.ReactNode;
+	title: string;
+	widthSeparator: number;
+}) {
+	let width: string;
+
+	switch (widthSeparator) {
+		case 1:
+			width = '50%';
+			break;
+		case 2:
+			width = '75%';
+			break;
+		case 3:
+			width = '100%';
+			break;
+		default:
+			width = '';
+	}
+
+	return (
+		<div className="py-2">
+			<h1 className={`${merriweather.className} font-bold text-2xl`}>
+				{title}
+			</h1>
+			<div className="flex items-center pb-2">
+				<div
+					className={`h-1 bg-black`}
+					style={{
+						width,
+					}}
+				/>
+				<div className="h-2 w-2 bg-black rounded-full -translate-x-1" />
+			</div>
+			<div className="pl-5">{children}</div>
+		</div>
+	);
+}
+
+export default Section;
