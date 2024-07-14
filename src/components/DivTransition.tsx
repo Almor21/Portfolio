465eff45e3267ac6668a8d1ae8fcc497e3919224
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-07-14 02:11:50 -0500

    feat: add component for transitions between pages

---
+++ src/components/DivTransition.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import React from 'react';
+import { motion } from 'framer-motion';
+
+function DivTransition({
+	children,
+	className,
+}: {
+	children: React.ReactNode;
+	className?: string;
+}) {
+	return (
+		<motion.div
+			className={className}
+			initial={{ x: -10, opacity: 0 }}
+			animate={{ x: 0, opacity: 1 }}
+			transition={{
+				delay: 0.3,
+				duration: 0.4,
+			}}
+		>
+			{children}
+		</motion.div>
+	);
+}
+
+export default DivTransition;
