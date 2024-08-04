c81c5b52ddaa4638983068c7f486c08a78e69565
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-08-03 23:11:07 -0500

    feat: add responsive behavior to Background component

--- src/components/Backgroud.tsx
+++ src/components/Backgroud.tsx
@@ -1,34 +1,54 @@
 'use client';
 
 import { useStoreBackground } from '@/stores/useStore';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 
 function Backgroud() {
 	const XBackground = useStoreBackground((state) => state.XBackground);
 	const divRef = useRef<HTMLDivElement>(null);
-	const widthRef = useRef(0);
+	const [axis, setAxis] = useState(0);
 
 	useEffect(() => {
-		const screenWidth = window.screen.width;
-		const excess = screenWidth - 1440 > 0 ? screenWidth - 1440 : 0;
-		widthRef.current = (excess) / 2;
+		const updateSize = () => {
+			const screenWidth = window.screen.width;
+
+			if (screenWidth > 768) {
+				setAxis(0);
+			} else {
+				setAxis(1);
+			}
+		};
+		updateSize();
+
+		window.addEventListener('resize', updateSize);
+
+		return () => {
+			window.removeEventListener('resize', updateSize);
+		};
 	}, []);
 
 	return (
 		<motion.div
-			className="absolute h-full top-0 left-0 -z-50 bg-black"
+			className="absolute md:h-full max-md:w-full top-0 left-0 -z-50 bg-black"
 			ref={divRef}
-			initial={{
-				width: widthRef.current,
-			}}
-			animate={{
-				width:
-					XBackground.at(-1) === '%'
-						? XBackground
-						: parseInt(XBackground) -
-						  (divRef.current?.getBoundingClientRect().left ?? 0),
-			}}
+			animate={
+				axis === 0
+					? {
+							width:
+								XBackground.at(-1) === '%'
+									? XBackground
+									: parseInt(XBackground),
+							height: '100%',
+					  }
+					: {
+							width: '100%',
+							height:
+								XBackground.at(-1) === '%'
+									? XBackground
+									: parseInt(XBackground),
+					  }
+			}
 			transition={{
 				type: 'spring',
 				duration: 0.8,
