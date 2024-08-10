ab352b8f1ba91404868eceaade031fe35f47a308
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-08-10 14:02:00 -0500

    feat[skills]: create animations for links component in tactile devices

--- src/components/contact/Links.tsx
+++ src/components/contact/Links.tsx
@@ -2,33 +2,121 @@
 
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import { useAnimate } from 'framer-motion';
+import { motion, useAnimate } from 'framer-motion';
+import useIsTouchDevice from '@/hook/useIsTouchDevice';
 
 function Links() {
 	const [scope, animate] = useAnimate();
 	const [hover, setHover] = useState(-1);
+	const [enterView, setEnterView] = useState(false);
+	const [colorImage1, setColorImage1] = useState(false);
+	const [colorImage2, setColorImage2] = useState(false)
+	const isTouch = useIsTouchDevice();
+
+	const runAnimation = (
+		num: number,
+		type: 'active' | 'noactive',
+		delay: number = 0
+	) => {
+		if (type === 'active') {
+			let color = '';
+			switch (num) {
+				case 0:
+					color = '0,0,0';
+					break;
+				case 1:
+					color = '255,0,0';
+					break;
+				case 2:
+					color = '0,0,255';
+					break;
+			}
+
+			animate(
+				`#div${num}`,
+				{
+					backgroundColor: `rgba(${color},0.08)`,
+					borderColor: `rgba(${color},0.1)`,
+					boxShadow: `0 0 10px 5px rgba(${color},0.08)`,
+				},
+				{ delay }
+			);
+			animate(`#div${num} .image`, { y: -3 }, { delay });
+			animate(`#div${num} .mask`, { scaleX: 0 }, { delay });
+			setTimeout(() => {
+				if (num === 1) {
+					setColorImage1(true)
+				} else if (num === 2) {
+					setColorImage2(true)
+				}
+			}, delay * 1000);
+		} else if (type === 'noactive') {
+			animate(
+				`#div${num}`,
+				{
+					backgroundColor: `rgba(255,255,255,0)`,
+					borderColor: `rgba(0,0,0,0.3)`,
+					boxShadow: `none`,
+				},
+				{ delay }
+			);
+			animate(`#div${num} .image`, { y: 0 }, { delay });
+			animate(`#div${num} .mask`, { scaleX: 1 }, { delay });
+			setTimeout(() => {
+				
+				if (num === 1) {
+					setColorImage1(false)
+				} else if (num === 2) {
+					setColorImage2(false)
+				}
+			}, delay * 1000);
+		}
+	};
 
 	useEffect(() => {
 		if (hover === -1) return;
 
-		animate(`#div${hover} .image`, { y: -3 });
-		animate(`#div${hover} .mask`, { scaleX: 0 });
+		runAnimation(hover, 'active');
 
 		return () => {
-			animate(`#div${hover} .image`, { y: 0 });
-			animate(`#div${hover} .mask`, { scaleX: 1 });
+			runAnimation(hover, 'noactive');
 		};
 	}, [hover]);
 
+	useEffect(() => {
+		if (enterView) {
+			for (let i = 0; i <= 2; i++) {
+				runAnimation(i, 'active', 0.5 + i * 0.2);
+			}
+		} else {
+			for (let i = 0; i <= 2; i++) {
+				runAnimation(i, 'noactive');
+			}
+		}
+	}, [enterView]);
+
+	useEffect(() => {
+		setHover(-1);
+		setEnterView(false);
+	}, [isTouch]);
+
 	return (
-		<div
+		<motion.div
 			ref={scope}
 			className="relative flex px-5 justify-between items-center"
+			onViewportEnter={() => {
+				if (isTouch) setEnterView(true);
+			}}
+			onViewportLeave={() => {
+				setEnterView(false);
+			}}
 		>
 			<div
 				id="div0"
-				className="h-14 w-14 p-2 backdrop-blur-sm border border-[rgba(0,0,0,0.3)] shadow-none rounded cursor-pointer transition-all hover:bg-[rgba(0,0,0,0.08)] hover:border-[rgba(0,0,255,0.1)] hover:shadow-[0_0_10px_5px_rgba(0,0,0,0.08)]"
-				onMouseEnter={() => setHover(0)}
+				className="h-14 w-14 p-2 backdrop-blur-sm border border-[rgba(0,0,0,0.3)] shadow-none rounded cursor-pointer"
+				onMouseEnter={() => {
+					if (!isTouch) setHover(0);
+				}}
 				onMouseLeave={() => setHover(-1)}
 			>
 				<div className="image relative h-full w-full z-10">
@@ -41,8 +129,10 @@ function Links() {
 			</div>
 			<div
 				id="div1"
-				className="relative h-14 w-14 p-2 backdrop-blur-sm border border-[rgba(0,0,0,0.3)] shadow-none rounded cursor-pointer transition-all hover:bg-[rgba(255,0,0,0.08)] hover:border-[rgba(255,0,0,0.1)] hover:shadow-[0_0_10px_5px_rgba(255,0,0,0.08)]"
-				onMouseEnter={() => setHover(1)}
+				className="relative h-14 w-14 p-2 backdrop-blur-sm border border-[rgba(0,0,0,0.3)] shadow-none rounded cursor-pointer"
+				onMouseEnter={() => {
+					if (!isTouch) setHover(1);
+				}}
 				onMouseLeave={() => setHover(-1)}
 			>
 				<div className="image relative h-full w-full">
@@ -52,7 +142,7 @@ function Links() {
 						alt="i"
 						fill
 						style={
-							hover === 1
+							colorImage1
 								? {
 										filter: 'invert(66%) sepia(16%) saturate(5848%) hue-rotate(316deg) brightness(100%) contrast(93%)',
 								  }
@@ -67,18 +157,20 @@ function Links() {
 			</div>
 			<div
 				id="div2"
-				className="h-14 w-14 p-2 backdrop-blur-sm border border-[rgba(0,0,0,0.3)] shadow-none rounded cursor-pointer transition-all hover:bg-[rgba(0,0,255,0.08)] hover:border-[rgba(0,0,255,0.1)] hover:shadow-[0_0_10px_5px_rgba(0,0,255,0.08)]"
-				onMouseEnter={() => setHover(2)}
+				className="h-14 w-14 p-2 backdrop-blur-sm border border-[rgba(0,0,0,0.3)] shadow-none rounded cursor-pointer"
+				onMouseEnter={() => {
+					if (!isTouch) setHover(2);
+				}}
 				onMouseLeave={() => setHover(-1)}
 			>
 				<div className="image relative h-full w-full">
 					<Image
-						src={'/icons/Linkedin.svg'}
 						className="transition-all"
+						src={'/icons/Linkedin.svg'}
 						alt="i"
 						fill
 						style={
-							hover === 2
+							colorImage2
 								? {
 										filter: 'invert(59%) sepia(78%) saturate(1022%) hue-rotate(187deg) brightness(95%) contrast(108%)',
 								  }
@@ -91,7 +183,7 @@ function Links() {
 					<span className="mask absolute inline-block top-0 left-0 bg-white z-10 h-full w-full origin-right" />
 				</div>
 			</div>
-		</div>
+		</motion.div>
 	);
 }
 
