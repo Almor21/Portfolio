b37850580ba601f9de55fc57db84d5d415780e4d
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-08-17 18:59:41 -0500

    feat[contact]: add empty field validation

--- src/components/contact/Form.tsx
+++ src/components/contact/Form.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import SendButton from './SendButton';
+import { animate } from 'framer-motion';
 
 function Form() {
 	const [name, setName] = useState('');
@@ -9,6 +10,37 @@ function Form() {
 	const [message, setMessage] = useState('');
 
 	const sendAction = async () => {
+		let empty = [];
+		if (!name) empty.push('name');
+		if (!email) empty.push('email');
+		if (!message) empty.push('message');
+
+		empty.forEach(async (v) => {
+			await animate(
+				`#input_${v}`,
+				{
+					borderColor: 'rgb(230,0,0)',
+					color: 'rgb(230,0,0)',
+				},
+				{
+					duration: 0.2,
+				}
+			);
+
+			await new Promise((resolve) => setTimeout(resolve, 200));
+
+			await animate(
+				`#input_${v}`,
+				{
+					borderColor: 'rgb(255,255,255)',
+					color: 'rgb(255,255,255)',
+				},
+				{ duration: 1 }
+			);
+		});
+
+		if (empty.length !== 0) return 'failed';
+
 		const status = await fetch('/api/email', {
 			method: 'POST',
 			headers: {
@@ -24,19 +56,21 @@ function Form() {
 		if (status === 200) {
 			return 'complete';
 		} else {
-			return 'failed'
+			return 'failed';
 		}
 	};
 
 	return (
-		<form className="flex flex-col gap-3">
+		<form id="form_contact" className="flex flex-col gap-3">
 			<input
+				id="input_name"
 				className="w-full p-2 bg-black text-white border-[1.4px] border-white focus:outline-none"
 				placeholder="Name"
 				value={name}
 				onChange={(e) => setName(e.target.value)}
 			/>
 			<input
+				id="input_email"
 				className="w-full p-2 bg-black text-white border-[1.4px] border-white focus:outline-none"
 				type="email"
 				placeholder="Email"
@@ -44,6 +78,7 @@ function Form() {
 				onChange={(e) => setEmail(e.target.value)}
 			/>
 			<textarea
+				id="input_message"
 				className="w-full p-2 bg-black text-white border-[1.4px] border-white focus:outline-none resize-y h-32 max-h-40 min-h-14"
 				placeholder="Message"
 				value={message}
