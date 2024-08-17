ed801861c9c6e3f34b89375cbeb67042578d65f8
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-08-17 18:25:27 -0500

    refactor: add option to different sizes to Loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-function Loader({className}: {className?: string}) {
+function Loader({ className, size = 2 }: { className?: string, size?: number }) {
+	let realSize = '0';
+	switch (size) {
+		case 1:
+			realSize = '0.5rem'
+			break;
+		case 2:
+			realSize = '0.75rem'
+			break;
+	}
+
 	return (
 		<div className={`${className} flex justify-center items-center`}>
 			<div className="relative flex gap-1">
 				{Array.from({ length: 3 }, (v, i) => i).map((index) => (
 					<motion.span
 						key={index}
-						className="inline-block h-3 w-3 rounded-full bg-black"
+						className="inline-block rounded-full bg-black"
+						style={{
+							height: realSize,
+							width: realSize
+						}}
 						animate={{
 							y: [0, '-100%', 0],
 						}}
