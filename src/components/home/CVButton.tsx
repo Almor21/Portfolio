a41faa59510da393ebb737c17e9b16f056d8def5
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-09-08 22:06:57 -0500

    style: change general styles to CVButton component

--- src/components/home/CVButton.tsx
+++ src/components/home/CVButton.tsx
@@ -39,14 +39,14 @@ function CVButton() {
 						scaleX: 0,
 					},
 					{
-						duration: 0.3,
+						duration: 0.2,
 						ease: 'easeOut',
 					}
 				),
 				animate(
 					'#span_background',
 					{
-						backgroundColor: 'rgb(255,255,255)',
+						backgroundColor: 'rgb(0,0,0)',
 					},
 					{
 						duration: 0.2,
@@ -57,7 +57,7 @@ function CVButton() {
 				animate(
 					'#span_text',
 					{
-						color: 'rgb(0,0,0)',
+						color: 'rgb(255,255,255)',
 					},
 					{
 						duration: 0.2,
@@ -72,10 +72,10 @@ function CVButton() {
 					scaleX: 1,
 				}),
 				animate('#span_background', {
-					backgroundColor: 'rgb(0,0,0)',
+					backgroundColor: 'rgb(255,255,255)',
 				}),
 				animate('#span_text', {
-					color: 'rgb(255,255,255)',
+					color: 'rgb(0,0,0)',
 				}),
 			];
 		}
@@ -97,7 +97,7 @@ function CVButton() {
 
 	return (
 		<button
-			className="relative w-32 h-9 flex justify-center items-center rounded-full z-20 mix-blend-difference"
+			className="relative w-32 h-9 flex justify-center items-center rounded-full z-20"
 			ref={scope}
 			onClick={() => download()}
 			onMouseEnter={() => {
@@ -111,18 +111,18 @@ function CVButton() {
 				style={{
 					transformOrigin: 'right',
 				}}
-				className="absolute top-0 left-0 z-10 w-[50%] h-full bg-black"
+				className="absolute top-0 left-0 z-10 w-[50%] h-full bg-white"
 			/>
 			<span
 				id="span_background"
-				className="absolute top-0 w-full h-full border-2 border-white rounded-full"
+				className="absolute top-0 w-full h-full border-2 border-black rounded-full"
 			/>
-			<span id="span_text" className="relative font-light z-20">
+			<span id="span_text" className="relative z-20">
 				Download CV
 			</span>
 			<span
 				id="span_right"
-				className="absolute top-0 right-0 z-10 w-[50%] h-full bg-black"
+				className="absolute top-0 right-0 z-10 w-[50%] h-full bg-white"
 				style={{
 					transformOrigin: 'left',
 				}}
